d609ae2c4fbe556c3016e7c489e9f41b78eb4d97
HUYHOANG\nguye <nguyenhuyhoang765@gmail.com>
2023-04-27 09:37:06 +0700

    first commit

---
+++ font-end/src/App.js
@@ -0,0 +1,17 @@
+import LayoutDefault from "./layouts/LayoutDefault";
+import Home from "./pages/Home";
+
+
+function App() {
+  return (
+    
+    <>
+    <LayoutDefault>
+      <Home />
+    </LayoutDefault>
+
+    </>
+  );
+}
+
+export default App;
